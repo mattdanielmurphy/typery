0e9f70e628550d0295b5a9c5afa1e51cd679933c
Matt <matt@mattmurphy.ca>
2020-04-14 18:33:17 -0600

    merge?

--- containers/appContainer.js
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic'
-import React from 'react'
-
-const AppContainer = (props) => <React.Fragment>{props.children}</React.Fragment>
-	
-	export default dynamic(() => Promise.resolve(AppContainer), {
-			ssr: false
-	})
